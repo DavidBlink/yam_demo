3872e330caf51de4d7ff1aacd16b8f626a5898b2
DavidBlink <39801596+DavidBlink@users.noreply.github.com>
2019-01-18 13:04:34 +0100

    Update style.js

--- style.js
+++ style.js
@@ -49,6 +49,8 @@
 				{ name: 'Icono Hablar2', element: 'span', attributes: { 'class': 'icon icon-hablar2' } },
 				{ name: 'Icono Lapiz', element: 'span', attributes: { 'class': 'icon icon-lapiz' } },
 				{ name: 'Icono Musica', element: 'span', attributes: { 'class': 'icon icon-musica' } },
+				{ name: 'Icono MusicaMorado1', element: 'span', attributes: { 'class': 'icon icon-musica1' } },
+				{ name: 'Icono MusicaMorado2', element: 'span', attributes: { 'class': 'icon icon-musica2' } },
 				{ name: 'Icono Circulo', element: 'span', attributes: { 'class': 'icon icon-circulo' } },
 				{ name: 'Icono Cuadrados', element: 'span', attributes: { 'class': 'icon icon-cuadrados' } },
 
